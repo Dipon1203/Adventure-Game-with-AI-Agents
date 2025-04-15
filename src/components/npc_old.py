2f7c29c67545f4ed2c3bb8208abddb9b5c7f4b21
Dipon12 <dipontalukder12@gmail.com>
2025-04-15 21:37:21 +0600

    add: agent

--- src/components/npc_old.py
+++ src/components/npc_old.py
@@ -12,6 +12,43 @@ class NPC(Usable):
         from components.player import Player
         player = other.get(Player)
 
+        
+        print(f"Talking to {self.obj_name}")
+
+        if distance < npc_talk_distance:
+            file = open(npc_folder_location + "/" + self.npc_file, "r")
+            data = file.read()
+            file.close()
+            lines = data.split('\n')
+
+            print(lines)
+            
+            from components.ui.dialogue_view import DialogueView
+            DialogueView(lines, self, player)
+
+            
+        else:
+            player.show_message("I need to get closer")
+
+
+
+pass
+
+from components.usable import Usable
+from content.intelligent_npcs.npc_agent import NPCAgent
+
+npc_folder_location = "content/npcs"
+npc_talk_distance = 150
+
+class NPC(Usable):
+    def __init__(self, obj_name, npc_file):
+        super().__init__(obj_name)
+        self.npc_file = npc_file
+
+    def on(self, other, distance):
+        from components.player import Player
+        player = other.get(Player)
+
 
         print(f"Talking to {self.obj_name}")
 
@@ -33,5 +70,7 @@ class NPC(Usable):
             from components.ui.dialogue_view import DialogueView
             DialogueView(structured_response, self, player)
 
+            
         else:
             player.show_message("I need to get closer")
+
